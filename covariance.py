5ee3f7c5398347fc91154166cf2584ce2716ff16
umberto1992 <s3715736@studenti.unige.it>
2017-05-05 15:44:52 +0200

    added cov function

--- covariance.py
+++ covariance.py
@@ -1,26 +1,26 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 """
-Created on Wed May  3 12:10:07 2017
+Created on Fri May  5 11:35:47 2017
 
 @author: umberto
 """
 import numpy as np
 
-layer_HH = np.load('img00FullHH.npy')
-layer_VV = np.load('img00FullVV.npy')
-layer_HVVH = np.load('img00FullVH+HV.npy')
-
-full_image = np.zeros((2,3,5687,4101)).astype(np.int16)
-
-full_image[:,0,:,:] = layer_HH
-full_image[:,1,:,:] = layer_HVVH
-full_image[:,2,:,:] = layer_VV
-
-full_image_con = full_image
-full_image_con[1,:,:,:] = -full_image[1,:,:,:]
-
-#for i in range(1):
+def covariance(matrix, n_looks):
     
+    l_pixels = n_looks**2
+    rows = np.floor(np.size(matrix[0,0,:,0])/n_looks)
+    columns = np.floor(np.size(matrix[0,0,0,:])/n_looks)
+    covmat = np.zeros((3,3,2,rows,columns))
     
+    for i in range(rows):
+        
+        for iii in range(columns):
+            
+            covmat[0,0,0,i,iii] =
+            covmat[0,0,1,i,iii] =
+            covmat[1,0,0,i,iii] =
+            
     
+    return covmat
